fe4eb928ba9e3b101a5ac23f04584876e798300d
Jiří Pešík <jiri.pesik@outlook.cz>
2023-10-23 19:55:53 +0200

    Fix

--- zakladni-dotazy.py
+++ zakladni-dotazy.py
@@ -1,24 +1,5 @@
 import pandas
 
-#----------------------Jmena 1 -----------------------------------#
-jmena = pandas.read_csv("jmena.csv")
-jmena = jmena.set_index("jméno")
-print(jmena)
-
-# Vypište na konzoli informace o jménu Jiří.
-print(jmena.loc["Jiří"])
-# Vypište na konzoli informace pro jména Martin, Zuzana a Josef.
-print(jmena.loc[["Martin", "Zuzana", "Josef"]])
-# Vypište na konzoli informace o všech nejčastějších jménech až po Martina.
-print(jmena.loc[:"Martin"])
-# Vypište pouze průměrné věky osob mající jména mezi Martinem a Terezou.
-print(jmena.loc["Martin": "Tereza", "věk"])
-# Vypište průměrný věk a původ pro všechna jména od Libora dolů.
-print(jmena.loc["Libor":,["věk", "původ"]])
-# Vypište sloupečky mezi věkem a původem pro všechna jména v tabulce.
-print(jmena.loc[:,"věk":"původ"])
-
-
 #----------------------Jmena 2 -----------------------------------#
 jmena = pandas.read_csv("jmena.csv")
 # Vypiš všechny řádky se jmény, jejichž nositelé mají průměrný věk vyšší než 60.
